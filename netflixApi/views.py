5dd0fd8cc53ac30fe7c7d95a0bdbc323ce4fc003
farzana583 <farzanaisack@gmail.com>
2021-01-14 17:19:15 +0300

    add

---
+++ netflixApi/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.shortcuts import render
+import requests,json
+import os
+from decouple import config,Csv
+from googleapiclient.discovery import build
+
+# Create your views here.
+
+def home1(request,category):
+    key =  'd4489bcee40f582ad63c715aa123c3a5'  
+    url =  config('url') 
+    url2 = url.format(category,key)
+    urll = requests.get(url2)
+    urlll = urll.json()
+    return urlll
+
+def home(request):
+    popular = home1(request,'popular')
+    upcoming = home1(request,'upcoming')
+    latest = home1(request,'latest')
+    topRated = home1(request,'top_rated')
+
+    return render(request,'netflix.html',{'popular':popular,"upcoming":upcoming,"latest":latest,"toprated":topRated})
+
+def youtube(request,id):
+    yTubeKey =  config('yTubeKey')
+    popular = home1(request,'popular')
+    pp = ''
+    for p in popular['results']:
+        if str(p['id'])==str(id):
+            pp = p['title']
+    youtube = build('youtube','v3',developerKey = yTubeKey)
+    req = youtube.search().list(q= pp+'trailer',part = 'snippet',type= 'video')
+    res = req.execute()
+    return render(request,'youtube.html',{'response':res})
